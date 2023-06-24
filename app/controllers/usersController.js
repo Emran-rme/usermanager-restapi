576ec5a0bdc00e1e51c25241cc80b732d2a4243f
emran <rostamzadeemran93@gmail.com>
2023-06-25 01:05:13 +0330

    Ninth Commit - Read - the second operation crud - show users List

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -2,9 +2,14 @@ const userModel = require("../models/User");
 
 class User {
     async index(req, res, next) {
+        const users = await userModel.find({});
+
         res.status(200).json({
             success: "true",
             message: "لیست کاربران با موفقیت ایجاد شد",
+            data: {
+                users,
+            },
         });
     }
 
@@ -13,8 +18,13 @@ class User {
             const { first_name, last_name, mobile, email } = req.body;
 
             //validation
-            if (first_name.trim() === "" || last_name.trim() === "") {
-                res.status(422).json({
+            if (
+                first_name === undefined ||
+                last_name === undefined ||
+                first_name.trim() === "" ||
+                last_name.trim() === ""
+            ) {
+                return res.status(422).json({
                     error: true,
                     message: "اطلاعات ارسالی برای ایجاد کاربر معتبر نمی باشد",
                 });
