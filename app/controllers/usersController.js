16388457441bc3ecfcd192eb1a4d060c4d221975
emran <rostamzadeemran93@gmail.com>
2023-06-25 00:42:48 +0330

    Seventh Commit - Create, the first operation crud

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -1,9 +1,32 @@
+const userModel = require("../models/User");
+
 class User {
-    index(req, res) {
+    index(req, res, next) {
         res.json({
             success: "true",
             message: "لیست کاربران با موفقیت ایجاد شد",
         });
+    }
+
+    async store(req, res, next) {
+        try {
+            const newUser = new userModel({
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                mobile: req.body.mobile,
+                email: req.body.email,
+            });
+
+            await newUser.save();
+
+            res.status(201).json({
+                success: true,
+                message: "کاربر جدید با موفقیت ایجاد شد",
+                newUserID: newUser._id,
+            });
+        } catch (error) {
+            next(error);
+        }
     }
 }
 
--- app/routes/user.js
+++ app/routes/user.js
@@ -5,5 +5,6 @@ const usersController = require("../controllers/usersController");
 const router = express.Router();
 
 router.get("/", usersController.index);
+router.post("/", usersController.store);
 
 module.exports = router;
