13f26f9681e4dc0ce15ddba1b9cdaceae0a1d6cf
emran <rostamzadeemran93@gmail.com>
2023-06-24 11:47:39 +0330

    Second Commit - global middleware

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+require("./middleware")(app);
+
 module.exports = (port) => {
     app.listen(port, () => {
         console.log(`App is running on port: ${port}`);
